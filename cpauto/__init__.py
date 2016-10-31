7a9b94a99dd9cfd2c8b8f81aef28842a817737a0
Dana James Traversie <dana-at-cp@users.noreply.github.com>
2016-10-31 12:09:16 -0400

    Create __init__.py

---
+++ cpauto/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2016 Dana James Traversie and Check Point Software Technologies, Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+#  __   __   __        _|_   __  
+# (___ |__) (__( (__(_  |_, (__) 
+#      |                         
+#
+
+"""
+cpauto library
+~~~~~~~~~~~~~~~
+
+cpauto is a client library, written in Python, for the web APIs exposed
+via Check Point R80 management server software. The Check Point R80 management
+APIs provide automation and integration capabilities that were not available
+in previous versions of Check Point management server software.
+
+usage:
+
+TBD
+
+:copyright: (c) 2016 by Dana James Traversie and Check Point Software Technologies, Ltd.
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+__title__ = 'cpauto'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Dana James Traversie'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Dana James Traversie and Check Point Software Technologies, Ltd.'
