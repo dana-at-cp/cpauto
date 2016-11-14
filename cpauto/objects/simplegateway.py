88f713885fb2f43e791a1cc6c1a93310f06d08cc
dana-at-cp <dtravers@checkpoint.com>
2016-11-14 16:03:53 -0500

    Adds full simple gateway object support

--- cpauto/objects/simplegateway.py
+++ cpauto/objects/simplegateway.py
@@ -19,43 +19,90 @@
 
 """This module contains the classes needed to manage simple gateway objects."""
 
+from ._common import _CommonClient
+
 class SimpleGatewayClient:
     def __init__(self, core_client):
         self.__core_client = core_client
+        self.__common_client = _CommonClient(core_client)
 
-    def add(self, name, ip_address=None, ipv4_address=None, ipv6_address=None, params={}):
-        """Adds a simple gateway. Returns CoreClientResult object.
+    def add(self, name='', ip_address='', ipv4_address='', ipv6_address='', params={}):
+        """Adds a simple gateway.
+
+        https://sc1.checkpoint.com/documents/R80/APIs/#web/add-simple-gateway
 
         :param name: A name for the new simple gateway.
-        :param ip_address: IPv4 or IPv6 address. If both addresses are required use ipv4_address and ipv6_address fields explicitly.
+        :param ip_address: (optional) IPv4 or IPv6 address. If both addresses are
+            required use ipv4_address and ipv6_address fields explicitly.
         :param ipv4_address: (optional) IPv4 address.
         :param ipv6_address: (optional) IPv6 address.
-        :param params: (optional) A dictionary of additional, supported parameter names and values.
-        :rtype: cpauto.core.sessions.CoreClientResult
+        :param params: (optional) A dictionary of additional, supported
+            parameter names and values.
+        :rtype: CoreClientResult
         """
-        # https://sc1.checkpoint.com/documents/R80/APIs/#web/add-simple-gateway
         payload = { 'name': name }
-        if ip_address is not None:
+        if ip_address:
             payload['ip-address'] = ip_address
-        if ipv4_address is not None:
+        if ipv4_address:
             payload['ipv4-address'] = ipv4_address
-        if ipv6_address is not None:
+        if ipv6_address:
             payload['ipv6-address'] = ipv6_address
         if params:
             payload = self.__core_client.merge_payloads(payload, params)
         return self.__core_client.http_post('add-simple-gateway', payload=payload)
 
-    def delete(self, name=None, uid=None):
-        """Deletes a simple gateway. Returns CoreClientResult object.
+    def delete(self, name='', uid='', params={}):
+        """Deletes a simple gateway.
 
-        :param name: A simple gateway name.
+        https://sc1.checkpoint.com/documents/R80/APIs/#web/delete-simple-gateway
+
+        :param name: (optional) A simple gateway name.
         :param uid: (optional) A simple gateway unique identifier instead of name.
-        :rtype: cpauto.core.sessions.CoreClientResult
+        :rtype: CoreClientResult
         """
-        # https://sc1.checkpoint.com/documents/R80/APIs/#web/delete-simple-gateway
-        payload = {}
-        if uid is not None:
-            payload = { 'uid': uid }
-        if name is not None:
-            payload = { 'name': name }
-        return self.__core_client.http_post('delete-simple-gateway', payload=payload)
+        return self.__common_client._delete('delete-simple-gateway', name=name, uid=uid, params=params)
+
+    def show(self, name='', uid='', details_level=''):
+        """Shows details of a simple gateway with the specified name or unique
+        identifier.
+
+        https://sc1.checkpoint.com/documents/R80/APIs/#web/show-simple-gateway
+
+        :param name: (optional) A simple gateway name.
+        :param uid: (optional) A simple gateway unique identifier instead of name.
+        :param details_level: (optional) The level of detail to show. Default
+            value is 'standard' and the other options are: 'uid' or 'full'
+        :rtype: CoreClientResult
+        """
+        return self.__common_client._show('show-simple-gateway', name=name, uid=uid, details_level=details_level)
+
+    def set(self, name='', uid='', params={}):
+        """Sets new values for an existing simple gateway with the specified
+        name or uid.
+
+        https://sc1.checkpoint.com/documents/R80/APIs/#web/set-simple-gateway
+
+        :param name: (optional) The name of an existing simple gateway.
+        :param uid: (optional) The unique identifier of an existing simple gateway.
+        :param params: (optional) A dictionary of additional, supported parameter names and values.
+        :rtype: CoreClientResult
+        """
+        return self.__common_client._set('set-simple-gateway', name=name, uid=uid, params=params)
+
+    def show_all(self, limit=50, offset=0, order=[], details_level=''):
+        """Shows all simple gateways with some reasonable limitations.
+
+        https://sc1.checkpoint.com/documents/R80/APIs/#web/show-simple-gateways
+
+        :param limit: (optional) Limit the total number of simple gateways shown.
+            The default value is 50 and allowed values are in the range 1 to 500.
+        :param offset: (optional) Skip a number of simple gateways in the results
+            before they are shown. Default value is 0.
+        :param order: (optional) Sort the results by the specified field. The
+            default is a random order.
+        :param details_level: (optional) The level of detail to show. Default
+            value is 'standard' and the other options are: 'uid' or 'full'
+        :rtype: CoreClientResult
+        """
+        return self.__common_client._show_all('show-simple-gateways', limit=limit,
+            offset=offset, order=order, details_level=details_level)
---
+++ tests/objects/test_simplegateway.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for cpauto.objects.simplegateway module."""
+
+import pytest
+import responses
+import cpauto
+
+@pytest.mark.parametrize("name,ip_address,ipv4_address,ipv6_address,params", [
+    ("gw-2200", "192.168.1.1", "", "", {}),
+    ("gw-61K", '', "10.11.12.13", '2002:0a0b:0c0d::0a0b:0c0d', {}),
+    ("gw-vmware", "192.168.1.4", "", "", {"one-time-password": "password1"}),
+])
+def test_add(core_client, mgmt_server_base_uri, name, ip_address, ipv4_address, ipv6_address, params):
+    endpoint = mgmt_server_base_uri + 'add-simple-gateway'
+    with responses.RequestsMock() as rsps:
+        resp_body = {'foo': 'bar', 'message': 'OK'}
+        rsps.add(responses.POST, endpoint,
+                 json=resp_body, status=200,
+                 content_type='application/json')
+
+        c = cpauto.SimpleGatewayClient(core_client)
+        r = c.add(name=name, ip_address=ip_address,
+                  ipv4_address=ipv4_address, ipv6_address=ipv6_address,
+                  params=params)
+
+        assert r.status_code == 200
+        assert r.json() == resp_body
+
+@pytest.mark.parametrize("name,uid,details_level", [
+    ("gw-2200", "", ""),
+    ("", "somelonguid", ""),
+    ("gw-2200", "", "uid"),
+    ("", "somelonguid", "full"),
+])
+def test_show(core_client, mgmt_server_base_uri, name, uid, details_level):
+    endpoint = mgmt_server_base_uri + 'show-simple-gateway'
+    with responses.RequestsMock() as rsps:
+        resp_body = {'foo': 'bar', 'message': 'OK'}
+        rsps.add(responses.POST, endpoint,
+                 json=resp_body, status=200,
+                 content_type='application/json')
+
+        c = cpauto.SimpleGatewayClient(core_client)
+        r = c.show(name=name, uid=uid, details_level=details_level)
+
+        assert r.status_code == 200
+        assert r.json() == resp_body
+
+@pytest.mark.parametrize("name,uid,params", [
+    ("gw-2200", "", {'ip-address': '192.168.1.1', 'one-time-password': 'password1'}),
+    ("", "somelonguid", {'threat-emulation': 'True'}),
+])
+def test_set(core_client, mgmt_server_base_uri, name, uid, params):
+    endpoint = mgmt_server_base_uri + 'set-simple-gateway'
+    with responses.RequestsMock() as rsps:
+        resp_body = {'foo': 'bar', 'message': 'OK'}
+        rsps.add(responses.POST, endpoint,
+                 json=resp_body, status=200,
+                 content_type='application/json')
+
+        c = cpauto.SimpleGatewayClient(core_client)
+        r = c.set(name=name, uid=uid, params=params)
+
+        assert r.status_code == 200
+        assert r.json() == resp_body
+
+@pytest.mark.parametrize("name,uid,params", [
+    ("gw-2200", "", {}),
+    ("", "somelonguid", {}),
+    ("gw-2200", "", {'details-level': 'full'}),
+    ("", "somelonguid", {'ignore-errors': 'True'}),
+])
+def test_delete(core_client, mgmt_server_base_uri, name, uid, params):
+    endpoint = mgmt_server_base_uri + 'delete-simple-gateway'
+    with responses.RequestsMock() as rsps:
+        resp_body = {'foo': 'bar', 'message': 'OK'}
+        rsps.add(responses.POST, endpoint,
+                 json=resp_body, status=200,
+                 content_type='application/json')
+
+        c = cpauto.SimpleGatewayClient(core_client)
+        r = c.delete(name=name, uid=uid, params=params)
+
+        assert r.status_code == 200
+        assert r.json() == resp_body
+
+@pytest.mark.parametrize("limit,offset,order,details_level", [
+    (50, 0, [], ''),
+    (50, 0, [{'ASC': 'foo'}], ''),
+    (64, 32, [{'DESC': 'bar'}], 'uid'),
+])
+def test_show_all(core_client, mgmt_server_base_uri,
+    limit, offset, order, details_level):
+    endpoint = mgmt_server_base_uri + 'show-simple-gateways'
+    with responses.RequestsMock() as rsps:
+        resp_body = {'foo': 'bar', 'message': 'OK'}
+        rsps.add(responses.POST, endpoint,
+                 json=resp_body, status=200,
+                 content_type='application/json')
+
+        c = cpauto.SimpleGatewayClient(core_client)
+        r = c.show_all(limit=limit, offset=offset,
+                       order=order, details_level=details_level)
+
+        assert r.status_code == 200
+        assert r.json() == resp_body
